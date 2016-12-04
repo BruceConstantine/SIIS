62505a854f4ea70581b073d4570a5d937631dd90
lvhuiyang <i@lvhuiyang.cn>
2016-12-04 17:59:32 +0800

    spider init

--- config.py
+++ config.py
@@ -2,9 +2,6 @@ import os
 
 
 class Config:
-    def __init__(self):
-        pass
-
     SECRET_KEY = os.environ.get('SECRET_KEY')
     SQLALCHEMY_TRACK_MODIFICATIONS = True
     SQLALCHEMY_COMMIT_ON_TEARDOWN = True
--- manage.py
+++ manage.py
@@ -1,6 +1,7 @@
 from app import create_app, db
 from flask_script import Manager, Shell
 from flask_migrate import MigrateCommand, Migrate
+from spider import xhcx, zcjcx
 
 app = create_app('default')
 manager = Manager(app)
@@ -11,8 +12,10 @@ def make_shell_context():
     return dict(app=app, db=db)
 
 
-def make_crawl(obj):
-    return 'This is a test from %s', obj
+@manager.command
+def hello(stu_number):
+    print('HelloWorld %s' % stu_number)
+    xhcx.parse(xhcx.splider('1411'))
 
 
 manager.add_command('shell', Shell(make_context=make_shell_context))
